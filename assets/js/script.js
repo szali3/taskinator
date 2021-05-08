75541f680c61f7edf632f40427a9514d2119de26
Zohaib Ali <s.zohaibali3@gmail.com>
2021-05-08 13:18:03 -0400

    handle submission form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,16 @@
-var buttonE1 = document.querySelector("#save-task")
+
 var taskToDoE1 = document.querySelector("#task-to-do")
+var formE1= document.querySelector("#task-form")
 
 var createTaskHandler = function() {
+
+    event.preventDefault();
+
     var listItemE1 = document.createElement("li");
     listItemE1.className = "task-item";
     listItemE1.textContent = "This is a new task."
     taskToDoE1.appendChild(listItemE1);
 };
 
-buttonE1.addEventListener("click", createTaskHandler);
+
+formE1.addEventListener('submit', createTaskHandler);
